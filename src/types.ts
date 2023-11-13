fbbe2d62b1ef5feac5a5b80778fa02b25ff7d143
dilhann <dilhan.nathan@gmail.com>
2023-11-13 11:02:08 +0100

    TD3 - Exercice 6 - export interface

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface Utilisateur{
+    id: number;
+    adresseEmail: string;
+    login: string;
+    premium: boolean;
+}
+export interface Publication{
+    id: number;
+    message: string;
+    datePublication: string;
+    auteur: Utilisateur;
+}
