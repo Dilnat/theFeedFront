ccc81ce354f91215e51355d07f35cb3543869702
dilhann <dilhan.nathan@gmail.com>
2023-11-13 11:52:16 +0100

    TD3 - Exercice 7 - BoiteUtilisateur

--- src/views/AllUsers.vue
+++ src/views/AllUsers.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+  import type {Utilisateur} from "@/types";
+  import type {Ref} from "vue";
+  import {ref} from "vue";
+  import BoiteUtilisateur from "@/components/BoiteUtilisateur.vue";
+
+  const users:Ref<Utilisateur[]> = ref([{
+    id:4,
+    adresseEmail:"toto@gouv.fr",
+    login:"toto",
+    premium:false
+  },{
+    id:5,
+    adresseEmail:"tony@gouv.fr",
+    login:"tony",
+    premium:false
+  }]);
+</script>
+
+
 <template>
-  Ceci est la vue de la liste des utilisateurs.
+  <ul>
+    <li v-for="user in users"  :key="user.id">
+      <BoiteUtilisateur :utilisateur="user"></BoiteUtilisateur>
+    </li>
+  </ul>
 </template>
--- src/views/SingleUser.vue
+++ src/views/SingleUser.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
   import { useRoute } from 'vue-router'
+  import type {Utilisateur} from "@/types";
+  import BoiteUtilisateur from "@/components/BoiteUtilisateur.vue";
   const route = useRoute()
-  const id = route.params.id
+  const id = Number(route.params.id)
+  const user:Utilisateur = {
+    id:id,
+    adresseEmail:"toto@gouv.fr",
+    login:"toto",
+    premium:false
+  };
 </script>
 
 <template>
-  Ceci est la vue de l'utilisateur {{id}}
+  <BoiteUtilisateur :utilisateur="user"></BoiteUtilisateur>
 </template>
 
 <style scoped>
